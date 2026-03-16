5bff6a58b1b12f23cf3ec44c5e3742fb68d30846
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-16 11:25:49 +0100

    favourite

--- Java script execise/favourite.js
+++ Java script execise/favourite.js
@@ -1,6 +1,6 @@
 let favoriteColor = "Blue";
 let favoriteFood = "moi moi and pap";
-let favoriteSport = "Football";
-let favoriteMovie = "london has fallen";
+let favoriteSport = "football";
+let favoriteMovie = "legend of the seeker";
 
 console.log(`My favorite color is ${favoriteColor}, my favorite food is ${favoriteFood}, my favorite sport is ${favoriteSport}, and my favorite movie is ${favoriteMovie}.`);
