b738fc8f2cab22d54899e8a6c4304165f027b89f
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-16 13:06:32 +0100

    program

---
+++ Java script execise/program.js
@@ -0,0 +1,42 @@
+let word1 = "programming";
+
+if (word1.length < 5) {
+  console.log(word1 + ": short");
+} else if (word1.length >= 5 && word1.length <= 8) {
+  console.log(word1 + ": medium");
+} else {
+  console.log(word1 + ": long");
+}
+word2 = "hi";
+if (word2.length < 5) {
+  console.log(word2 + ": short");
+} else if (word2.length >= 5 && word2.length <= 8) {
+  console.log(word2 + ": medium");
+} else {
+  console.log(word2 + ": long");
+}
+word3 = "hello";
+if (word3.length < 5) {
+  console.log(word3 + ": short");
+} else if (word3.length >= 5 && word3.length <= 8) {
+  console.log(word3 + ": medium");
+} else {
+  console.log(word3 + ": long");
+}
+
+word4 = "javascript";
+if (word4.length < 5) {
+  console.log(word4 + ": short");
+} else if (word4.length >= 5 && word4.length <= 8) {
+  console.log(word4 + ": medium");
+} else {
+  console.log(word4 + ": long");
+}
+word5 = "language";
+if (word5.length < 5) {
+  console.log(word5 + ": short");
+} else if (word5.length >= 5 && word5.length <= 8) {
+  console.log(word5 + ": medium");
+} else {
+  console.log(word5 + ": long");
+}
