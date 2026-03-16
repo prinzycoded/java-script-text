9d522490d4ffa5eae34fdff7253f65052045d753
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-16 12:18:03 +0100

    circle.calculate

---
+++ Java script execise/circle.calculate.js
@@ -0,0 +1,8 @@
+let radius = 5;
+
+let area = Math.PI * radius * radius;
+
+let circumference = 2 * Math.PI * radius;
+
+console.log("Area:", area.toFixed(2));
+console.log("Circumference:", circumference.toFixed(2));
