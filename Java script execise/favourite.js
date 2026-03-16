4d145be24deee78f0f1bdcc145a8e278b8ff7cba
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-16 11:18:51 +0100

    first commit

---
+++ Java script execise/favourite.js
@@ -0,0 +1,6 @@
+let favoriteColor = "Blue";
+let favoriteFood = "moi moi and pap";
+let favoriteSport = "Football";
+let favoriteMovie = "london has fallen";
+
+console.log(`My favorite color is ${favoriteColor}, my favorite food is ${favoriteFood}, my favorite sport is ${favoriteSport}, and my favorite movie is ${favoriteMovie}.`);
